65009c32ea48bb91f0a449b502c0ad732bd0f0b2
KillPippo <killpippo@hotmail.it>
2016-08-21 12:04:18 +0200

    fixed product notify message client: products displayed in a table

--- OnlineShop/services/ProductQueueService.js
+++ OnlineShop/services/ProductQueueService.js
@@ -5,9 +5,24 @@ var productHandler = require('./ProductHandler');
 var aws = require('aws-sdk');
 
 function processMessage(message) {
-	logger.info('Product process message', message);
+	var messageBody, type;
+
+	var bodyParse = JSON.parse(message.Body);
+
+	type = bodyParse.Type || 'SQSMessage';
+
+	switch (type) {
+		case 'SQSMessage':
+			messageBody = message.Body;
+			break;
+		case 'Notification':
+			messageBody = bodyParse.Message;
+			break;
+	}
+
+	logger.info('Product process message', messageBody);
 	
-	var product = productHandler.parse(message.Body);
+	var product = productHandler.parse(messageBody);
 	
 	productHandler.notify(product);
 }
